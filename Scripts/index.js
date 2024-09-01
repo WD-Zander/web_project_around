9b94c0360395c01991c765ab593e59c1fa5e18f8
Zander J <zanderjosue05@gmail.com>
2024-09-01 19:34:55 -0400

    Modal_popup

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,14 +1,21 @@
+
+
+
 let editButton = document.querySelector(".section__edit");
 let formModal = document.querySelector(".form");
 let closeButton = document.querySelector(".form__close");
 let form = document.querySelector('.form');
 let inputName = document.querySelector('.form__input');
 let h1 = document.querySelector('.section__profile-info');
-let inputAbout = document.querySelector('.form__input-about')
-let h2 = document.querySelector('.section__profile-tag')
+let inputAbout = document.querySelector('.form__input-about');
+let h2 = document.querySelector('.section__profile-tag');
 
 function openModal() {
   formModal.style.display = "flex";
+  inputName.value=h1.textContent
+  inputAbout.value=h2.textContent
+
+
 }
 
 function closeModal() {
@@ -28,14 +35,11 @@ window.addEventListener("click", function (event) {
 form.addEventListener('submit', function (event) {
   event.preventDefault();
   let getName = inputName.value;
-
-
   let aboutMe = inputAbout.value;
   h1.textContent = getName;
-
-
   h2.textContent = aboutMe;
 
   closeModal();
 });
+
 
